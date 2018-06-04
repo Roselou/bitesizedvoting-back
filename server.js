1681fadd1b39ef5e9dd7ce042b57bf67a2a9cbfe
DaltonHart <daltonvaren@gmail.com>
2018-06-04 15:01:24 -0700

    set up mailer

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
+const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
 
 //Body-Parser 
-app.use(bodyParer.json());
+app.use(bodyParser.json());
 
 
 
@@ -21,7 +22,57 @@ app.get('/', function (req, res) {
     res.send('Yup, working...');
 })
 
+
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+            user: 'bitesizedvoter@gmail.com',
+            pass: 'Hackathon2018',
+    }
+});
+
+let mailOptions = {
+    from: 'bitesizedvoter@gmail.com',
+    to: 'daltonvaren@gmail.com',
+    subject: 'You Choose A Canidate!',
+    html: `
+    <html>
+    <head>
+      <style>
+        #body {
+          font-size: 14px;
+        }
+      </style>
+    </head>
+    <body>
+      <div id='body'>
+        <h2>Hi Voter!</h2>
+        <p>You learned about the SF Mayor candidates and you supported:</p>
+        <a href='#'><h2>Name of Canidate</h2></a>
+        <img src="http://www.clker.com/cliparts/i/2/n/G/S/J/blue-thumbs-up-icon.svg.med.png" height="45" width="45">
+        <a href='https://registertovote.ca.gov/'><h2>Registor to Vote</h2></a>
+        <p style="color:red">Vote June 6th</p>
+        <p style="font-weight: bold">Thanks,</p>
+        <p>Bite Sized Voter Team</p>
+      </div>
+    </body>
+  </html>
+    `
+}
+
+transporter.sendMail(mailOptions, function(err, res){
+    if(err){
+        console.log('Error');
+    } else {
+        console.log('Email Sent');
+    }
+});
+
+
+
 //Listener 
-app.lister(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 8080, function(){
     console.log("listening on port 8080!")
 })
